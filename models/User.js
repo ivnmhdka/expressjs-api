28d983d78e5b6e55dcd9b9793efc305c71a6f2a7
ivnmhdka <ivanmahadika05@gmail.com>
2022-11-23 07:30:51 +0700

    main

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  nama: {
+    type: String,
+    required: [true, "Ivan Mahadika Yanuarizqi"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "ivanmahadika05@gmail.com",
+    ],
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
